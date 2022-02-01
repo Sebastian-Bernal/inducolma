27788ef9622b6c5a25b531edc052c6f831470d5d
Oscar Tarapuez <4lveiro@gmail.com>
2022-02-01 10:43:04 -0500

    archivos costos-operacion

---
+++ resources/views/modulos/costos-operacion-edit.blade.php
@@ -0,0 +1,69 @@
+@extends('layout')
+
+@section('title', 'Editar operaci&oacute;n | Inducolma')
+
+@section('content')
+<form action="{{ route('costos-de-operacion.update', $costosOperacion->id) }}" method="POST" enctype="multipart/form-data">
+    @csrf
+    @method('PATCH')
+
+        
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Editar costo de operaci&oacute;n</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="input-group mb-3"> 
+                    <span class="input-group-text">Cantidad:</span>                            
+                    <input type="number" class="form-control" placeholder="Cantidad" step="0.01" name="cantidad" id="cantidad" required value="{{ $costosOperacion->cantidad }}">
+                </div>
+
+                <div class="input-group mb-3">
+                    <span class="input-group-text">Valor mes:</span>                               
+                    <input type="number" class="form-control" placeholder="valor mes" step="0.01" name="valorMes" id="valorMes" required value="{{ $costosOperacion->valor_mes }}">
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text">Valor dia:</span>                               
+                    <input type="number" class="form-control" placeholder="valor dia" step="0.01" name="valorDia" id="valorDia" required value="{{ $costosOperacion->valor_dia }}">
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text">Costo Kw/h:</span>                               
+                    <input type="number" class="form-control" placeholder="costo kw/h" step="0.01" name="costokwh" id="costokwh" required value="{{ $costosOperacion->costo_kwh }}">
+                </div>
+                
+                <div class="input-group mb-3">                               
+                    <select class="form-select form-select-sm" aria-label=".form-select-sm example" name="idMaquina" id="idMaquina">
+                        <option value="{{ $costosOperacion->maquina_id }}" selected>{{ $costosOperacion->maquina->maquina }}</option>
+                        @foreach ($maquinas as $maquina)
+                            <option value="{{ $maquina->id }}">{{ $maquina->maquina }}</option>
+                        @endforeach 
+                    </select>
+                </div>
+                <div class="input-group mb-3">                               
+                    <select class="form-select form-select-sm" aria-label=".form-select-sm example" name="idDescripcion" id="idDescripcion">
+                        <option value="{{ $costosOperacion->descripcion_id }}" selected>{{ $costosOperacion->descripcion->descripcion }}</option>
+                        @foreach ($descripciones as $descripcion)
+                            <option value="{{ $descripcion->id }}">{{ $descripcion->descripcion }}</option>
+                        @endforeach 
+                    </select>
+                </div>
+            </div>
+            @if ($errors->any())
+            <div class="alert alert-danger">
+                @foreach ($errors->all() as $error)
+                    - {{ $error }} <br>
+                @endforeach
+            </div>
+            
+        @endif
+            <div class="modal-footer">
+            <a href="{{ route('descripciones.index') }}" class="btn btn-secondary" >Volver</a>
+            <button type="submit" class="btn btn-primary">Modificar costo de operaci&oacute;n</button>
+            </div>
+        </div>
+        </div>
+      
+</form>
+@endsection
