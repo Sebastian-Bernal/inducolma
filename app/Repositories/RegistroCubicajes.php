6f3708b79e576d4239027d586f2752c88711223e
Oscar Alveiro Tarapuez Cuaical <4lveiro@gmail.com>
2022-04-12 12:15:06 -0500

    cubicaje, calculo de cm3 p2, p2x3mtrs

--- app/Repositories/RegistroCubicajes.php
+++ app/Repositories/RegistroCubicajes.php
@@ -17,6 +17,9 @@ class RegistroCubicajes
             $registro->largo = $cubicaje['largo'];
             $registro->ancho = $cubicaje['ancho'];
             $registro->alto = $cubicaje['alto'];
+            $registro->cm3 = $cubicaje['largo']*$cubicaje['ancho']*$cubicaje['alto'];
+            $registro->pulgadas_cuadradas = ($cubicaje['alto']/2.54)*($cubicaje['ancho']/2.54);
+            $registro->pulgadas_cuadradas_x3_metros = ($cubicaje['largo']/300)* (((integer)($cubicaje['alto']/2.54)) * ((integer)($cubicaje['ancho']/2.54)));
             $registro->bloque = $cubicaje['bloque'];
             $registro->entrada_madera_id = $cubicaje['entrada_id'];
             $registro->user_id = auth()->user()->id;
