e49babf64f3b3058d6a5b7c803ef42d1d308c480
Juan Bernal <givecc2@gmail.com>
2022-10-14 10:57:30 -0500

    collapse con media queries para eventos y estado de maquina 1410 am

--- resources/views/modulos/operaciones/trabajo-maquina/show.blade.php
+++ resources/views/modulos/operaciones/trabajo-maquina/show.blade.php
@@ -7,36 +7,44 @@
 @section('content')
 <div class="div container h-content ">
     <div class="d-flex flex-wrap row  m-auto align-items-center container-fluid ">
-       <div class="container col-xl-12 col-lg-12 col-md-12 col-sm-12 bg-light shadow rounded-3  min-vh-100 mb-2 mt-2 overflow-scroll"> 
-        <p class="d-flex align-items-center">
-            <button class="btn btn-warning text-light m-2 self-start" type="button" data-bs-toggle="collapse" data-bs-target="#estadoMaquina" aria-expanded="false" aria-controls="estadoMaquina">
-              ESTADOS DE LA MAQUINA
-            </button>
-            <button class="btn btn-primary text-light m-2 self-end" type="button" data-bs-toggle="collapse" data-bs-target="#eventoMaquina" aria-expanded="false" aria-controls="eventoMaquina">
-                EVENTOS DE LA MAQUINA
-              </button>
-          </p>
-          <div class="collapse" id="estadoMaquina">
-            <div class="card card-body">
-                <button type="button" class="btn col-sm-6 col-md-3 btn-primary m-2" onclick="estadoDeMaquina('ENCENDIDA')">ENCENDIDA</button>
-                <button type="button" class="btn col-sm-6 col-md-3 btn-warning m-2 " onclick="estadoDeMaquina('ASEO')">ASEO</button>
-                <button type="button" class="btn col-sm-6 col-md-3 btn-danger m-2" onclick="estadoDeMaquina('APAGADA')">APAGADA</button>
-                <button type="button" class="btn col-sm-6 col-md-3  btn-secondary m-2" onclick="estadoDeMaquina('MANTENIMIENTO')">MANTENIMIENTO</button>
-                <button type="button" class="btn col-sm-6 col-md-3 btn-dark m-2" onclick="estadoDeMaquina('REPARACION')">REPARACIÓN</button>
+       <div class="container col-xl-12 col-lg-12 col-md-12 col-sm-12 bg-light shadow rounded-3 min-vh-100 mb-2 mt-2 overflow-scroll"> 
+            <div class="d-flex flex-wrap align-items-center ">    
+                <div class="self-start col-md-6 col-sm-12 col-12 " >
+                    <button class="btn btn-warning text-light mt-2 mb-2 w-100 rounded-pill" type="button" data-bs-toggle="collapse" data-bs-target="#estadoMaquina" aria-expanded="false" aria-controls="estadoMaquina">
+                    ESTADOS DE LA MAQUINA
+                    </button>
+                    <div class="collapse " id="estadoMaquina">
+                        <div class="card card-body border border-warning border-4 rounded-3">
+                            <button type="button" class="btn w-80 btn-primary text-light m-2" onclick="estadoDeMaquina('ENCENDIDA')">ENCENDIDA</button>
+                            <button type="button" class="btn w-80 btn-secondary text-light m-2 " onclick="estadoDeMaquina('ASEO')">ASEO</button>
+                            <button type="button" class="btn w-80  btn-secondary text-light m-2" onclick="estadoDeMaquina('MANTENIMIENTO')">MANTENIMIENTO</button>
+                            <button type="button" class="btn w-80 btn-secondary text-light m-2" onclick="estadoDeMaquina('REPARACION')">REPARACIÓN</button>
+                            <button type="button" class="btn w-80 btn-danger text-light m-2" onclick="estadoDeMaquina('APAGADA')">APAGADA</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="self-end col-md-6 col-sm-12 col-12">
+                    <button class="btn btn-primary text-light mt-2 mb-2 w-100 rounded-pill" type="button" data-bs-toggle="collapse" data-bs-target="#eventoMaquina" aria-expanded="false" aria-controls="eventoMaquina">
+                        EVENTOS DE LA MAQUINA
+                    </button>
+                    <div class="collapse " id="eventoMaquina">
+                        <div class="card card-body border border-primary border-4 rounded-3">
+                            <button type="button" class="btn w-80 btn-warning text-light m-2">EVENTO DE LA MAQUINA</button>
+                            <button type="button" class="btn w-80 btn-warning text-light m-2">EVENTO DE PRODUCCIÓN</button>
+                            <button type="button" class="btn w-80 btn-warning text-light m-2">EVENTO DE OPERARIO</button>
+                            <button type="button" class="btn w-80 btn-warning text-light m-2">EVENTO ADMINISTRATIVO</button>
+                        </div>
+                    </div>
+                </div>
+                     
+                       
             </div>
-          </div>
-          <div class="collapse" id="eventoMaquina">
-            <div class="card card-body">
-                <button type="button" class="btn col-sm-6 col-md-3 btn-primary m-2">EVENTO DE LA MAQUINA</button>
-                <button type="button" class="btn col-sm-6 col-md-3 btn-warning m-2">EVENTO DE PRODUCCIÓN</button>
-                <button type="button" class="btn col-sm-6 col-md-3 btn-danger m-2">EVENTO DE OPERARIO</button>
-                <button type="button" class="btn col-sm-6 col-md-3 btn-secondary m-2">EVENTO ADMINISTRATIVO</button>
-            </div>
-          </div>
+        
             <div class="text-primary">
                 <h1 class="display-6">Ordenes de produccion pendientes</h1>
                 <hr>
             </div>
+          
 
             <!-- Tabla -->
             <div >
