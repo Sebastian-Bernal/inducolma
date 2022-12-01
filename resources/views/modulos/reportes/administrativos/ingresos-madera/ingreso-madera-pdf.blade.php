108df6a4a304402e7156188fbfd55aefa58ad9ea
Juan Bernal <givecc2@gmail.com>
2022-11-30 23:02:59 -0500

    cambios en diseño de reporte maderas tamaño carta

--- resources/views/modulos/reportes/administrativos/ingresos-madera/ingreso-madera-pdf.blade.php
+++ resources/views/modulos/reportes/administrativos/ingresos-madera/ingreso-madera-pdf.blade.php
@@ -11,7 +11,7 @@ trtr<!DOCTYPE html>
     {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"  integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> --}}
   
     <body>
-        <p class="px-2 py-2 text-center rounded" style=" background-color:#fb8c00; color: white;">
+        <p class="px-2 py-2 text-center rounded mx-auto col-12" style=" background-color:#fb8c00; color: white;">
             <strong>{{ $encabezado }}</strong>
        
         </p>
@@ -20,40 +20,32 @@ trtr<!DOCTYPE html>
             <div>
                 @foreach ($data as $madera)
                 
-                <div class="row col-10 m-auto mt-1 mb-2 rounded p-1 border border-ligth" style="page-break-inside:avoid;" >
+                <div class="row col-12 m-auto mt-1 mb-2 rounded p-1 border border-ligth" style="page-break-inside:avoid;" >
                     <div class="mt-1 mx-auto" style="background-color:#649f21;">
-                        <p class="col-10 text-white m-1 mx-auto" style="font-size: 10px;"> Viaje No. {{ $madera->entrada_madera_id }}&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp; 
-                            &nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;Entidad Vigilante:  {{ $madera->entidad_vigilante }}
-                            &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp; 
-                            &nbsp; &nbsp;&nbsp; - &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;Mes: {{ $madera->mes }}  &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp; 
-                            &nbsp; &nbsp;&nbsp; - &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp; Año: {{ $madera->ano }}
-                            &nbsp; &nbsp;&nbsp;  &nbsp;&nbsp; &nbsp; 
-                            &nbsp; &nbsp;&nbsp; - &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;Fecha de Ingreso:  {{ $madera->fecha }}
-        
-                        
+                        <p class="col-12 text-white m-1 mx-auto" style="font-size: 10px;"> Viaje No. {{ $madera->entrada_madera_id }}
+                            &nbsp;&nbsp;  - &nbsp;Entidad Vigilante:  {{ $madera->entidad_vigilante }}
+                            &nbsp;&nbsp; &nbsp;-  &nbsp;&nbsp; &nbsp;Mes: {{ $madera->mes }}  
+                            &nbsp;&nbsp; &nbsp;- &nbsp;&nbsp; &nbsp; Año: {{ $madera->ano }}
+                            &nbsp;&nbsp; &nbsp; - &nbsp;&nbsp; &nbsp;Fecha de Ingreso:  {{ $madera->fecha }}
                         </p>
                     </div>
-                    <div class="col-12 mt-3">
-                        <label class="col-1 px-1 py-1" style="color:#649f21; font-size:10px;">PROVEEDOR:</label>
-                        
-                        <label class="col-2 rounded  py-1 px-1" style="font-size:10px; background-color:#cccccc;">{{ $madera->proveedor }}</label>
-                        
-                        <label class="col-1 px-1 py-1" style="color:#649f21; font-size:10px;">ACTO-ADMON:</label>
-                        
-                        <label class="col-2 rounded py-1 px-1" style="font-size:10px; background-color:#cccccc;">{{ $madera->acto_administrativo }}</label>
-                        <label class="col-2 px-1 py-1" style="color:#649f21; font-size:10px;">SALVOCNDUCTO:</label>
-                        
-                        <label class="col-2 rounded py-1 px-1" style="font-size:10px; background-color:#cccccc;">{{ $madera->salvoconducto_remision }}</label>
+                    <div class="col-12 mt-3 mx-auto">
+                        <label class="col-4 px-1 py-1" style="color:#649f21; font-size:10px;">PROVEEDOR:</label>
+                        <label class="col-4 px-1 py-1" style="color:#649f21; font-size:10px;">ACTO-ADMON:</label>
+                        <label class="col-3 px-1 py-1" style="color:#649f21; font-size:10px;">SALVOCNDUCTO:</label>
+                    </div>
+                    <div class="col-12 mx-auto">
+                        <label class="col-4 rounded  py-1 px-1" style="font-size:10px; background-color:#cccccc;">{{ $madera->proveedor }}</label>
+                        <label class="col-4 rounded py-1 px-1" style="font-size:10px; background-color:#cccccc;">{{ $madera->acto_administrativo }}</label>
+                        <label class="col-3 rounded py-1 px-1" style="font-size:10px; background-color:#cccccc;">{{ $madera->salvoconducto_remision }}</label>
                     </div>
                     <div class="col-12 mt-1">
-                        <label class="col-2 px-1 py-1" style="color:#649f21; font-size:10px;">TITULAR SALVOCONDUCTO:</label>
-                        
-                        <label class="col-2 rounded  py-1 px-1" style="font-size:10px; background-color:#cccccc;">{{ $madera->titular_salvoconducto }}</label>
-                        
-                        <label class="col-1 px-1 py-1" style="color:#649f21; font-size:10px;">CIUDAD:</label>
-                        
-                        <label class="col-5 rounded py-1 px-1" style="font-size:10px; background-color:#cccccc;">{{ $madera->procedencia_madera }}</label>
-                       
+                        <label class="col-5 px-1 py-1" style="color:#649f21; font-size:10px;">TITULAR SALVOCONDUCTO:</label>
+                        <label class="col-6 px-1 py-1" style="color:#649f21; font-size:10px;">CIUDAD:</label>
+                    </div>
+                    <div class="col-12 mx-auto">
+                        <label class="col-5 rounded  py-1 px-1" style="font-size:10px; background-color:#cccccc;">{{ $madera->titular_salvoconducto }}</label>
+                        <label class="col-6 rounded py-1 px-1" style="font-size:10px; background-color:#cccccc;">{{ $madera->procedencia_madera }}</label>
                     </div>
                     <div class="col-10 mt-1 mx-auto">
                         <p style="background-color:#fb8c00; min-height:2px;"></p>
