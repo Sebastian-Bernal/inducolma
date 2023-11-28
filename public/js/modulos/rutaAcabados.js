902809031ba83d65471bd75dcff4bf758c74ad27
Juan Bernal <givecc2@gmail.com>
2023-11-28 13:01:38 -0500

    vista de ruta acabados de ensamble

---
+++ public/js/modulos/rutaAcabados.js
@@ -0,0 +1,433 @@
+var rutasAcabados = [];
+var numProceso = 0;
+var cantidad_r = 0
+var pedido_id = $('#pedido_id').val();
+var cantidad_p = $('#cantidad_p').val();
+var cliente_rs = $('#cliente_rs').val();
+var diseno_name = $('#diseno_name').val();
+// Agregar valores al local storage
+comprobarLocalStorage()
+
+function comprobarLocalStorage() {
+    if (localStorage.getItem('rutasAcabados') == null || localStorage.getItem('rutasAcabados') == '[]') {
+        rutasAcabados = [];
+    } else {
+        rutasAcabados = JSON.parse(localStorage.getItem('rutasAcabados'));
+        listarProcesos(rutasAcabados);
+    }
+}
+
+function guardarAcabado(entra, sale, maquina, idMaquina, observa, tipo_corte, cantidad_r) {
+
+    let coincide = rutasAcabados.filter(rutasAcabados => rutasAcabados.maquina == maquina).length
+    if (coincide > 0) {
+        Swal.fire({
+            title: '¡Proceso ya fue definido!',
+            icon: 'warning',
+            confirmButtonColor: '#597504',
+            confirmButtonText: 'OK'
+        });
+
+    } else {
+        numProceso++;
+        let rutaNum = numProceso;
+        registroProceso = Object.assign({}, { rutaNum, entra, sale, maquina, idMaquina, observa, tipo_corte, pedido_id, cliente_rs, cantidad_r });
+        rutasAcabados.unshift(registroProceso);
+        localStorage.setItem('rutasAcabados', JSON.stringify(rutasAcabados));
+        listarProcesos(rutasAcabados);
+    };
+};
+// Funciones de validacion de campos en formularios
+
+function agregaRutaEnsamble() {
+    let valido, proceso = 'Ensamble', campoValor = 0;
+    let campos = $('#agregarEnsamble').find('select');
+    let textarea = $('#agregarEnsamble').find('textarea');
+    let entra, sale, maquina, observa, idMaquina;
+    let maquinaria = document.getElementById("maquinaensamble");
+    let tipo_corte = 'ENSAMBLE';
+    entra = ''
+    sale = ''
+    maquina = ''
+    idMaquina = 0
+    $.each(campos, function (index, value) {
+        campoValor = value.value
+        if (campoValor == 0) {
+            camposVacios()
+
+        } else {
+            if (value.id == 'entraEnsamble') {
+                entra = value.value
+            }
+            if (value.id == 'saleEnsamble') {
+                sale = value.value
+            }
+            if (value.id == 'maquinaensamble') {
+                maquina = maquinaria.options[maquinaria.selectedIndex].text
+                idMaquina = value.value
+            }
+            if (value.id == 'CantidadEnsamble') {
+                cantidad_r = value.value 
+            }
+        }
+    });
+    if (entra == 0 || sale == 0 || maquina == 0) {
+        campoValor = 0
+    }
+    let observaciones = $("#observacionEnsamble").val()
+    if (observaciones == '') {
+        Swal.fire({
+            title: '¡No contiene observaciones, desea dejar así!',
+            icon: 'warning',
+            confirmButtonColor: '#597504',
+            confirmButtonText: 'Si',
+            showDenyButton: true,
+            denyButtonText: 'No'
+
+        }).then((result) => {
+            if (result.isConfirmed) {
+                if (campoValor != 0) {
+                    valido = true;
+                    observa = ''
+                    validar(valido, proceso, entra, sale, maquina, idMaquina, observa, tipo_corte, cantidad_r)
+                } else {
+                    camposVacios()
+                    valido = false;
+                    validar(valido, proceso, entra, sale, maquina, idMaquina, observa, tipo_corte , cantidad_r)
+                }
+            } else {
+                valido = false;
+                validar(valido, proceso, entra, sale, maquina, idMaquina, observa, tipo_corte , cantidad_r)
+            }
+        });
+    }
+    if (observaciones != '' && campoValor != 0) {
+        valido = true;
+        observa = observaciones
+        validar(valido, proceso, entra, sale, maquina, idMaquina, observa, tipo_corte , cantidad_r)
+    }
+
+};
+
+function agregaRutaAcabadoEnsamble() {
+    let valido, proceso = 'AcabadoEnsamble', campoValor = 0;
+    let campos = $('#agregarAcabadoEnsamble').find('select');
+    let textarea = $('#agregarAcabadoEnsamble').find('textarea');
+    let entra, sale, maquina, observa, idMaquina;
+    let maquinaria = document.getElementById("maquinaAcabadoensamble");
+    let tipo_corte = 'ACABADO_ENSAMBLE';
+    entra = ''
+    sale = ''
+    maquina = ''
+    idMaquina = 0
+    $.each(campos, function (index, value) {
+        campoValor = value.value
+        if (campoValor == 0) {
+            camposVacios()
+
+        } else {
+            if (value.id == 'entraAcabadoEnsamble') {
+                entra = value.value
+            }
+            if (value.id == 'saleAcabadoEnsamble') {
+                sale = value.value
+            }
+            if (value.id == 'maquinaAcabadoensamble') {
+                maquina = maquinaria.options[maquinaria.selectedIndex].text
+                idMaquina = value.value
+            }
+            if (value.id == 'CantidadAcabadoEnsamble') {
+                cantidad_r = value.value 
+            }
+        }
+    });
+    if (entra == 0 || sale == 0 || maquina == 0) {
+        campoValor = 0
+    }
+    let observaciones = $("#observacionAcabadoEnsamble").val()
+    if (observaciones == '') {
+        Swal.fire({
+            title: '¡No contiene observaciones, desea dejar así!',
+            icon: 'warning',
+            confirmButtonColor: '#597504',
+            confirmButtonText: 'Si',
+            showDenyButton: true,
+            denyButtonText: 'No'
+
+        }).then((result) => {
+            if (result.isConfirmed) {
+                if (campoValor != 0) {
+                    valido = true;
+                    observa = ''
+                    validar(valido, proceso, entra, sale, maquina, idMaquina, observa, tipo_corte, cantidad_r)
+                } else {
+                    camposVacios()
+                    valido = false;
+                    validar(valido, proceso, entra, sale, maquina, idMaquina, observa, tipo_corte , cantidad_r)
+                }
+            } else {
+                valido = false;
+                validar(valido, proceso, entra, sale, maquina, idMaquina, observa, tipo_corte , cantidad_r)
+            }
+        });
+    }
+    if (observaciones != '' && campoValor != 0) {
+        valido = true;
+        observa = observaciones
+        validar(valido, proceso, entra, sale, maquina, idMaquina, observa, tipo_corte , cantidad_r)
+    }
+
+};
+
+
+
+// Validacion de datos correcta - procede a enviar a localStorage
+function validar(valido, proceso, entra, sale, maquina, idMaquina, observa, tipo_corte, cantidad_r) {
+    if (valido) {
+        console.log("envia datos");
+        procesoEjecutado(proceso)
+        guardarAcabado(entra, sale, maquina, idMaquina, observa, tipo_corte, cantidad_r)
+    } else {
+        console.log("no envia datos");
+        procesoEjecutado(proceso)
+    }
+}
+
+// define que formulario se debe limpiar
+function procesoEjecutado(proceso) {
+    if (proceso == 'Ensamble') {
+        limpiarEnsamble()
+    }
+    if (proceso == 'AcabadoEnsamble') {
+        limpiarIntermedia()
+    }
+}
+
+// Limpiar Inputs en formularios
+function limpiarEnsamble() {
+    $('#entraEnsamble').val('0');
+    $('#saleEnsamble').val('0');
+    $('#maquinaensamble').val('0');
+    $('#observacionEnsamble').val('');
+    $('#CantidadEnsamble').val('0');
+    $('#entraEnsamble').focus();
+};
+function limpiarIntermedia() {
+    $('#entraIntermedia').val('0');
+    $('#saleIntermedia').val('0');
+    $('#maquinaIntermedia').val('0');
+    $('#observacionIntermedia').val('');
+    $('#entraIntermedia').focus();
+};
+function limpiarFinal() {
+    $('#entraFinal').val('0');
+    $('#saleFinal').val('0');
+    $('#maquinaFinal').val('0');
+    $('#observacionFinal').val('');
+    $('#entraFinal').focus();
+};
+function limpiarAcabados() {
+    $('#entraAcabados').val('0');
+    $('#saleAcabados').val('0');
+    $('#maquinaAcabados').val('0');
+    $('#observacionAcabados').val('');
+    $('#entraAcabados').focus();
+};
+
+//Mensaje de campos vacios en select
+function camposVacios() {
+
+    Swal.fire({
+        title: '¡Ingrese todos los datos!',
+        icon: 'warning',
+        confirmButtonColor: '#597504',
+        confirmButtonText: 'OK'
+    });
+    valido = false;
+
+}
+
+// funcion listarProcesos, recibe un array de objetos y los muestra en la tabla
+function listarProcesos(rutasAcabados) {
+    $('#listarProcesos').html('');
+    rutasAcabados.forEach(rutasAcabados => {
+        let fila = `<tr id ="${rutasAcabados.rutaNum}">
+                        <td>${rutasAcabados.rutaNum}</td>
+                        <td>${rutasAcabados.maquina}</td>
+                        <td>${rutasAcabados.entra}</td>
+                        <td>${rutasAcabados.sale}</td>
+                        <td>${rutasAcabados.observa}</td>
+                        <td><button type="button" class="btn btn-danger" onclick="eliminarMadera(${rutasAcabados.rutaNum})"><i class="fas fa-trash-alt"></i></button></td>
+                    </tr>`;
+        $('#listarProcesos').append(fila);
+
+    })
+
+}
+
+function eliminarMadera(rutaNum) {
+
+    Swal.fire({
+        title: '¿Está seguro que desea eliminar el proceso?',
+        text: "¡No podrá revertir esta acción!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#597504',
+        cancelButtonColor: '#ccc',
+        confirmButtonText: '¡Si, eliminar!',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        //$(`#${id}`).remove();
+        if (result.isConfirmed) {
+            rutasAcabados = rutasAcabados.filter(rutasAcabados => rutasAcabados.rutaNum != rutaNum);
+            localStorage.setItem('rutasAcabados', JSON.stringify(rutasAcabados));
+            listarProcesos(rutasAcabados);
+        }
+    })
+}
+
+function guardarRutaBD() {
+
+    $.ajax({
+        url: '/procesos',
+        data: {
+            proceso: rutasAcabados,
+            _token: $('input[name="_token"]').val()
+        },
+        type: 'post',
+        success: function (guardado) {
+            if (guardado.error == false) {
+                Swal.fire({
+                    title: guardado.message,
+                    icon: 'success',
+                    confirmButtonColor: '#597504',
+                    confirmButtonText: 'OK'
+                })
+                    .then(() => {
+                        rutasAcabados = [];
+                        numBloque = 0;
+                        localStorage.removeItem('rutasAcabados');
+                        window.location.href = '/procesos';
+
+                    })
+            } else {
+                Swal.fire({
+                    title: guardado.message,
+                    icon: 'error',
+                    confirmButtonColor: '#597504',
+                    confirmButtonText: 'OK'
+                })
+            }
+        }
+    })
+
+}
+/**
+ * terminarRuta(), muestra cuadro de confirmacion para que el usuario acepte o no enviar los datos
+ * a la base de datos
+ */
+function terminarRuta() {
+    if (cubicajes.length > 0) {
+
+        Swal.fire({
+            title: '¿Está seguro que desea terminar la ruta?',
+            text: "¡No podrá revertir esta acción!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#597504',
+            cancelButtonColor: '#ff7e00',
+            confirmButtonText: '¡Si, terminar!',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                guardarRutaBD();
+            }
+        })
+    } else {
+        swal.fire({
+            title: '¡La ruta no tiene procesos agregados, no se puede terminar!',
+            icon: 'warning',
+            confirmButtonColor: '#597504',
+            confirmButtonText: 'OK'
+        })
+    }
+}
+
+/**
+ * terminarRuta(), evalua si rutasAcabados contiene datos, para poder guardar en la BD
+ * @returns {swal} mensaje de alerta
+ */
+function terminarRuta() {
+    if (rutasAcabados.length > 0) {
+        //guardarPaquetaBD();
+        Swal.fire({
+            title: "¿Está seguro de terminar la creacion de la ruta de programación?",
+            text: "¡No podrá revertir esta acción!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#597504",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "¡Si, terminar!",
+            cancelButtonText: "Cancelar",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                guardarRutaBD();
+            }
+        });
+    } else {
+        swal.fire({
+            title: "¡No hay una ruta creada, no puede terminar!",
+            icon: "warning",
+            confirmButtonColor: "#597504",
+            confirmButtonText: "OK",
+        });
+    }
+}
+/**
+ * guardarRutaBD(), envia la ruta seleccionada para ser guardada en la base de datos
+ * @param {Object} rutasAcabados
+ * @return {Swal}, retorna un mensaje Swal con mensaje de exito o error
+ */
+function guardarRutaBD() {
+    $.ajax({
+        url: "/procesos",
+        data: {
+            proceso: rutasAcabados,
+            _token: $('input[name="_token"]').val(),
+        },
+        type: "post",
+        success: function (guardado) {
+            console.log(guardado);
+            if (guardado.error == false) {
+                Swal.fire({
+                    title: guardado.mensaje,
+                    icon: "success",
+                    confirmButtonColor: "#597504",
+                    confirmButtonText: "OK",
+                }).then(() => {
+                    rutasAcabados = [];
+                    localStorage.removeItem("rutasAcabados");
+                    window.location.href = "/programaciones/"+pedido_id;
+                });
+            } else {
+                Swal.fire({
+                    title: guardado.mensaje,
+                    icon: "error",
+                    confirmButtonColor: "#597504",
+                    confirmButtonText: "OK",
+                });
+            }
+        },
+        error: function (error) {
+            console.log(error.status);
+            swal.fire({
+                title: error.statusText +' '+ error.status ,
+                text:  'Error al insertar los datos, por favor comuniquese con el admistrador de la aplicación',
+                icon: "error",
+                confirmButtonColor: "#597504",
+                confirmButtonText: "OK",
+            });
+        },
+    });
+}
--- resources/views/modulos/administrativo/ruta-acabado-producto/create.blade.php
+++ resources/views/modulos/administrativo/ruta-acabado-producto/create.blade.php
@@ -2,39 +2,222 @@
 @section('title', ' Pedidos | Inducolma')
 
 @section('submenu')
-    @include('modulos.sidebars.costos-side')
+@include('modulos.sidebars.costos-side')
 @endsection
 @section('content')
-    <div class="div container h-content ">
-        <div class="row">
-            <div class="col-12 col-sm-10 col-lg-6 mx-auto">
+<div class="div container h-content ">
+    <div class="row">
+        <div class="col-12 col-sm-10 col-lg-6 mx-auto">
+                    {{-- INPUTS HIDDEN --}}
+        <input type="hidden" name="pedido_id" value="{{ $pedido->id }}" id="pedido_id" readonly>
+        <input type="hidden" name="cantidad_p" value="{{ $pedido->cantidad }}" id="cantidad_p" readonly>
+        <input type="hidden" name="cliente_rs" value="{{ $pedido->cliente->razon_social }}" id="cliente_rs" readonly >
+        <input type="hidden" name="diseno_name" value="{{ $pedido->diseno_producto_final->descripcion }}" id="diseno_name" readonly>
 
-                <span> Id del ususario para enviar en json: User: {{ Auth::user()->id }}</span>
+            {{-- Id del ususario para enviar en json: User: {{ Auth::user()->id }} --}}
+            <h2 class="text-center text-primary"><strong>Crear ruta de acabados para el producto : {{
+                    $pedido->diseno_producto_final->descripcion }}</strong></h2>
+            <h4>
+                <p class="text-center m-1">
+                    <label><strong class="text-warning">PEDIDO No.:</strong> {{ $pedido->id }} / <strong class="text-warning">CLIENTE:</strong>  {{ $pedido->cliente->razon_social }}</label>
 
-                <h4 class="display-8" >
-                    Crear ruta de acabados para el producto : {{ $pedido->diseno_producto_final->descripcion }} <br>
-                    pedido: {{ $pedido->id }} <br>
-                    cliente: {{ $pedido->cliente->razon_social }} <br>
-                    cantidad : {{ $pedido->cantidad }}
-                </h4>
-                <span>Maquinas ensamble</span>
-                <select name="" id="">
-                    @foreach ($maquinas['ENSAMBLE'] as $ensamble)
-                        <option value="{{ $ensamble['id'] }}">{{ $ensamble['maquina'] }}</option>
-                    @endforeach
-                </select>
+                    <label><strong class="text-warning">CANTIDAD:</strong>  {{ $pedido->cantidad }}</label>
+                </p>
+            </h4>
 
-                <span>Maquinas acabado ensamble</span>
-                <select name="" id="">
-                    @foreach ($maquinas['ACABADO_ENSAMBLE'] as $acabado)
-                        <option value="{{ $acabado['id'] }}">{{ $acabado['maquina'] }}</option>
-                    @endforeach
-                </select>
+
+
         </div>
+        <div class="d-flex flex-wrap row-col-sm-1 row-col-md-2 container justify-content-center ">
+
+            {{-- ENSANBLE tarjeta --}}
+            <div class="container-sm col-sm-auto card  bg-light shadow m-3 rounded-3">
+                <div class=" card-header bg-primary text-white text-center">
+                    <strong>Maquina de Ensamble</strong>
+                </div>
+                <div class=" card-body">
+
+                    <div class="text-center">
+                        <label> Entrada de Material </label>
+                    </div>
+                    <form id="agregarEnsamble">
+                        <div class="input-group mb-3 mt-3">
+
+                            <label class="input-group-text" for="entraEnsamble">Entrada</label>
+                            <select class="form-select" id="entraEnsamble">
+                                <option value="0">Seleccionar...</option>
+                                <option value="Item Diseño">Item Diseño</option>
+                            </select>
+                        </div>
+                        <div class="text-center">
+                            <label> Salida de Material </label>
+                        </div>
+                        <div class="input-group mb-3 mt-3">
+
+                            <label class="input-group-text" for="saleEnsamble">Salida</label>
+                            <select class="form-select" id="saleEnsamble">
+                                <option value="0">Seleccionar...</option>
+                                <option value="Estiba">Estiba</option>
+                                <option value="Tablero">Tablero</option>
+                                <option value="Carreto">Carreto</option>
+
+                            </select>
+                        </div>
+                        <div class="mb-3 text-center">
+                            <label for="CantidadEnsamble" class="form-label text-center">Cantidad a
+                                Construir</label>
+                            <input type="number" class="form-control" id="CantidadEnsamble" name="CantidadEnsamble"></input>
+                        </div>
+                        <div class="text-center">
+                            <label> Proceso </label>
+
+                        </div>
+
+                        <div class="input-group mb-3 mt-3">
+
+                            <label class="input-group-text" for="maquinaensamble">Maquina</label>
+                            <select class="form-select" id="maquinaensamble">
+                                @isset($maquinas['ENSAMBLE'])
+                                <option value="0">Seleccionar...</option>
+                                @foreach ($maquinas['ENSAMBLE'] as $ensamble)
+
+                                <option value="{{ $ensamble['id'] }}" name="{{ $ensamble['maquina'] }}">{{
+                                    $ensamble['maquina']}}</option>
+
+                                @endforeach
+                                @else
+                                <option value="0">No existen maquinas de ensamble</option>
+                                @endisset
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="observacionEnsamble" class="form-label text-center">Observaciones</label>
+                            <textarea class="form-control" id="observacionEnsamble" rows="3"></textarea>
+                        </div>
+                    </form>
+                    <div>
+                        <button class=" bg-secondary text-center text-white container p-2 rounded"
+                            onClick="agregaRutaEnsamble()">Agregar a rutas</button>
+                    </div>
+                </div>
+            </div>
+
+            {{--ACABADOS DE ENSANBLE tarjeta --}}
+            <div class="container-sm col-sm-auto card  bg-light shadow m-3 rounded-3">
+                <div class=" card-header bg-warning text-white text-center">
+                    <strong>Maquina de Acabados del Diseño</strong>
+                </div>
+                <div class=" card-body">
+
+                    <div class="text-center">
+                        <label> Entrada de Material </label>
+                    </div>
+                    <form id="agregarAcabadoEnsamble">
+                        <div class="input-group mb-3 mt-3">
+
+                            <label class="input-group-text" for="entraAcabadoEnsamble">Entrada</label>
+                            <select class="form-select" id="entraAcabadoEnsamble">
+                                <option value="0">Seleccionar...</option>
+                                <option value="Estiba">Estiba</option>
+                                <option value="Tablero">Tablero</option>
+                                <option value="Carreto">Carreto</option>
+                            </select>
+                        </div>
+                        <div class="text-center">
+                            <label> Salida de Material </label>
+                        </div>
+                        <div class="input-group mb-3 mt-3">
+
+                            <label class="input-group-text" for="saleAcabadoEnsamble">Salida</label>
+                            <select class="form-select" id="saleAcabadoEnsamble">
+                                <option value="0">Seleccionar...</option>
+                                <option value="Estiba">Estiba</option>
+                                <option value="Tablero">Tablero</option>
+                                <option value="Carreto">Carreto</option>
+
+                            </select>
+                        </div>
+                        <div class="mb-3 text-center">
+                            <label for="CantidadAcabadoEnsamble" class="form-label text-center">Cantidad a
+                                Construir</label>
+                            <input type="number" class="form-control" id="CantidadAcabadoEnsamble"></input>
+                        </div>
+                        <div class="text-center">
+                            <label> Proceso </label>
+
+                        </div>
+
+                        <div class="input-group mb-3 mt-3">
+
+                            <label class="input-group-text" for="maquinaAcabadoensamble">Maquina</label>
+                            <select class="form-select" id="maquinaAcabadoensamble">
+                                @isset($maquinas['ACABADO_ENSAMBLE'])
+                                <option value="0">Seleccionar...</option>
+                                @foreach ($maquinas['ACABADO_ENSAMBLE'] as $acabado_ensamble)
+
+                                <option value="{{ $acabado_ensamble['id'] }}" name="{{ $acabado_ensamble['maquina'] }}">
+                                    {{ $acabado_ensamble['maquina']}}
+                                </option>
+
+                                @endforeach
+                                @else
+                                <option value="0">No existen maquinas de ensamble</option>
+                                @endisset
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="observacionAcabadoEnsamble" class="form-label text-center">Observaciones</label>
+                            <textarea class="form-control" id="observacionAcabadoEnsamble" rows="3"></textarea>
+                        </div>
+                    </form>
+                    <div>
+                        <button class=" bg-secondary text-center text-white container p-2 rounded"
+                            onClick="agregaRutaAcabadoEnsamble()">Agregar a rutas</button>
+                    </div>
+                </div>
+                
+            </div>
+            
+        </div>
+        <br>
+                <br>
+                <hr>
+                <br>
+                <br>
+                <div>
+                    <div class="text-center text-primary">
+                        <h2>Rutas Asignadas</h2>
+                        <br>
+                        <br>
+                    </div>
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">No.</th>
+                                <th scope="col">Maquina</th>
+                                <th scope="col">Entrada</th>
+                                <th scope="col">Salida</th>
+                                <th scope="col">Observacion</th>
+                                <th scope="col">Eliminar</th>
+                            </tr>
+                        </thead>
+                        <tbody id="listarProcesos">
+
+                        </tbody>
+                    </table>
+                </div>
+                <div>
+                    <button type="button" class="btn btn-primary container-fluid" onclick="terminarRuta()">Guardar ruta
+                        de
+                        programación</button>
+                </div>
     </div>
+
+</div>
+</div>
 
 @endsection
 
 @section('js')
-<script src="/js/modulos/programaciones.js"></script>
+<script src="/js/modulos/rutaAcabados.js"></script>
 @endsection
